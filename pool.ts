b86136fd7c09ca40344393c164ecbeb8536c422d
allush <al.lushnikov@yandex.ru>
2025-04-16 14:19:44 +0100

    add lp hoder's balance

--- pool.ts
+++ pool.ts
@@ -1,78 +1,66 @@
-import {ChainId, Symbiosis, Token} from "symbiosis-js-sdk";
+import {ChainId, Symbiosis, Token, TokenAmount} from "symbiosis-js-sdk";
 
 const symbiosis = new Symbiosis('mainnet', 'rootstock');
 
 const TRACKED_TOKENS = [
     new Token({
-        name: 'Tether USD',
-        symbol: 'rUSDT',
-        address: '0xef213441a85df4d7acbdae0cf78004e1e486bb96',
-        chainId: ChainId.RSK_MAINNET,
+        symbol: 'USDC',
+        address: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
+        chainId: ChainId.BSC_MAINNET,
         decimals: 18,
-        icons: {
-            large: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
-            small: 'https://s2.coinmarketcap.com/static/img/coins/128x128/825.png',
-        },
     }),
     new Token({
-        name: 'Wrapped RBTC',
-        symbol: 'WRBTC',
-        address: '0x542fda317318ebf1d3deaf76e0b632741a7e677d',
-        chainId: ChainId.RSK_MAINNET,
-        decimals: 18,
-        icons: {
-            large: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
-            small: 'https://s2.coinmarketcap.com/static/img/coins/128x128/1.png',
-        },
+        symbol: 'USDT',
+        address: '0xa614f803b6fd780986a42c78ec9c7f77e6ded13c',
+        chainId: ChainId.TRON_MAINNET,
+        decimals: 6,
     }),
 ]
 
+const LP_HOLDER = 'paste you address here in hex form'
+
 async function main() {
-    const promises = symbiosis.config.omniPools.map(async (poolConfig) => {
-        const tokens = symbiosis.getOmniPoolTokens(poolConfig)
-        const token = TRACKED_TOKENS.find((trackedToken) => {
-            const rep = symbiosis.getRepresentation(trackedToken, poolConfig.chainId)
+    const data = []
+
+    for (const poolConfig of symbiosis.config.omniPools) {
+        const poolTokens = symbiosis.getOmniPoolTokens(poolConfig)
+        for (const token of TRACKED_TOKENS) {
+            const rep = symbiosis.getRepresentation(token, poolConfig.chainId)
             if (!rep) {
-                return false
+                continue
             }
-            return !!tokens.find((i) => i.equals(rep))
-        })
-        if (!token) {
-            return
-        }
-        const rep = symbiosis.getRepresentation(token, poolConfig.chainId)
-        if (!rep) {
-            return false
-        }
-        const index = symbiosis.getOmniPoolTokenIndex(poolConfig, rep)
-        const poolContract = symbiosis.omniPool(poolConfig)
-        const asset = await poolContract.indexToAsset(index)
+            if (!poolTokens.find((i) => i.equals(rep))) {
+                continue
+            }
+            const index = symbiosis.getOmniPoolTokenIndex(poolConfig, rep)
+            const poolContract = symbiosis.omniPool(poolConfig)
+            const asset = await poolContract.indexToAsset(index)
 
-        const precision = Math.pow(10, 4)
+            const balance = await poolContract.balanceOf(LP_HOLDER, index)
 
-        return {
-            token,
-            tvl: parseFloat(asset.liability.mul(precision).div(1e18.toString()).toString()) / precision,
-            diff: parseFloat(asset.cash.sub(asset.liability).mul(precision).div(1e18.toString()).toString()) / precision,
-            percent: asset.liability.eq(0) ? 0 : (parseFloat(asset.cash.mul(10000).div(asset.liability).toString()) / 100 - 100),
+            const precision = Math.pow(10, 4)
+
+            data.push({
+                token,
+                tvl: parseFloat(asset.liability.mul(precision).div(1e18.toString()).toString()) / precision,
+                diff: parseFloat(asset.cash.sub(asset.liability).mul(precision).div(1e18.toString()).toString()) / precision,
+                percent: asset.liability.eq(0) ? 0 : (parseFloat(asset.cash.mul(10000).div(asset.liability).toString()) / 100 - 100),
+                balance: new TokenAmount(token, balance.toString()),
+            })
         }
-    })
-
-    const results = await Promise.allSettled(promises)
-
-    const data = results.map((item) => {
-        if (item.status !== 'fulfilled') {
-            return
-        }
-        return item.value
-    }).filter(Boolean)
+    }
 
 
     console.log(data.map((i) => {
         if (!i) {
             return
         }
-        return `TVL=${i.tvl} ${i.token.symbol}, DIFF=${i.diff} ${i.token.symbol} | ${i.percent.toFixed(2)}%`
+        return {
+            tvl: `${i.tvl} ${i.token.symbol}`,
+            diff_abs: `${i.diff} ${i.token.symbol}`,
+            diff_percent: `${i.percent.toFixed(2)}%`,
+            balance: `${i.balance.toSignificant()} ${i.token.symbol}`,
+        }
     }))
 }
 
