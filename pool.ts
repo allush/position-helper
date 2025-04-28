b23424d33d0a227adf86297b72323386e3b35740
allush <al.lushnikov@yandex.ru>
2025-04-28 18:32:03 +0300

    upd

--- pool.ts
+++ pool.ts
@@ -1,30 +1,19 @@
-import {ChainId, Symbiosis, Token, TokenAmount} from "symbiosis-js-sdk";
+import {Symbiosis, TokenAmount} from "symbiosis-js-sdk";
 
+import { BigNumber } from '@ethersproject/bignumber'
 const symbiosis = new Symbiosis('mainnet', 'rootstock');
 
-const TRACKED_TOKENS = [
-    new Token({
-        symbol: 'USDC',
-        address: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
-        chainId: ChainId.BSC_MAINNET,
-        decimals: 18,
-    }),
-    new Token({
-        symbol: 'USDT',
-        address: '0xa614f803b6fd780986a42c78ec9c7f77e6ded13c',
-        chainId: ChainId.TRON_MAINNET,
-        decimals: 6,
-    }),
-]
+const LP_HOLDER = '0x...'
 
-const LP_HOLDER = 'paste you address here in hex form'
+const WAD = BigNumber.from(10).pow(18)
 
 async function main() {
     const data = []
 
+    const trackedTokens = symbiosis.tokens()
     for (const poolConfig of symbiosis.config.omniPools) {
         const poolTokens = symbiosis.getOmniPoolTokens(poolConfig)
-        for (const token of TRACKED_TOKENS) {
+        for (const token of trackedTokens) {
             const rep = symbiosis.getRepresentation(token, poolConfig.chainId)
             if (!rep) {
                 continue
@@ -38,32 +27,38 @@ async function main() {
 
             const lpBalance = await poolContract.balanceOf(LP_HOLDER, index)
 
+            if (asset.liability.eq(0) || asset.cash.eq(0)) {
+                continue
+            }
+            const decimals = BigNumber.from(10).pow(token.decimals)
             const share = lpBalance
                 .mul(asset.liability)
                 .div(asset.totalSupply)
+                .mul(decimals)
+                .div(WAD)
 
             const precision = Math.pow(10, 4)
 
             data.push({
                 token,
-                tvl: parseFloat(asset.liability.mul(precision).div(1e18.toString()).toString()) / precision,
-                diff: parseFloat(asset.cash.sub(asset.liability).mul(precision).div(1e18.toString()).toString()) / precision,
+                tvl: parseFloat(asset.liability.mul(precision).div(WAD).toString()) / precision,
+                diff: parseFloat(asset.cash.sub(asset.liability).mul(precision).div(WAD).toString()) / precision,
                 percent: asset.liability.eq(0) ? 0 : (parseFloat(asset.cash.mul(10000).div(asset.liability).toString()) / 100 - 100),
-                balance: new TokenAmount(token, share.toString()),
+                balance: new TokenAmount(rep, share.toString()),
             })
         }
     }
 
-
-    console.log(data.map((i) => {
+    console.log(data.sort((a, b) => b.balance.greaterThan('0') ? 1 : -1).map((i) => {
         if (!i) {
             return
         }
         return {
-            tvl: `${i.tvl} ${i.token.symbol}`,
-            diff_abs: `${i.diff} ${i.token.symbol}`,
+            token: `${(i.token.chainFrom || i.token.chain)?.name}.${i.token.symbol}`,
+            tvl: `${i.tvl} `,
+            diff_abs: `${i.diff}`,
             diff_percent: `${i.percent.toFixed(2)}%`,
-            balance: `${i.balance.toSignificant()} ${i.token.symbol}`,
+            balance: `${i.balance.toSignificant()}`,
         }
     }))
 }
